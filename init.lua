6a19327704efe31a56dfc4869290bf5555931682
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2023-09-07 22:26:08 -0300

    feat(gitsigns): change git blame config

--- init.lua
+++ init.lua
@@ -104,8 +104,8 @@ require("lazy").setup({
       current_line_blame = true,
       current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
-        delay = 0,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 4000,
         ignore_whitespace = false,
       },
     }
